1d3e5d73a77e19333609e45a327c9e0e6099c44b
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 09:12:59 +0400

    add attributes into post class

--- src/main/java/com/Social_Networking_Platform/Social_Networking_Platform/Models/Post.java
+++ src/main/java/com/Social_Networking_Platform/Social_Networking_Platform/Models/Post.java
@@ -1,4 +1,23 @@
 package com.Social_Networking_Platform.Social_Networking_Platform.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "post")
 public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long post_id;
+    Integer content;
+    String posted_date;
+    Integer likes_count;
+    Integer comments_count;
+    
 }
