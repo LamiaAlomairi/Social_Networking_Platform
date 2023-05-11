cda19e81dc02bcda620c7d86181043d9cca5e4d4
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-11 09:44:27 +0400

    add attributes into messaging and follower classes

--- src/main/java/com/Social_Networking_Platform/Social_Networking_Platform/Models/Follower.java
+++ src/main/java/com/Social_Networking_Platform/Social_Networking_Platform/Models/Follower.java
@@ -1,4 +1,19 @@
 package com.Social_Networking_Platform.Social_Networking_Platform.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "follower")
 public class Follower {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long follower_id;
+
 }
--- src/main/java/com/Social_Networking_Platform/Social_Networking_Platform/Models/Messaging.java
+++ src/main/java/com/Social_Networking_Platform/Social_Networking_Platform/Models/Messaging.java
@@ -1,4 +1,21 @@
 package com.Social_Networking_Platform.Social_Networking_Platform.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "messaging")
 public class Messaging {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long messaging_id;
+    String content;
+    String sent_date;
+    
 }
